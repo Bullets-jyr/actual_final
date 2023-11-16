2e7b6215c179484ffdabd19bc225091b08762561
Bullets-jyr <jyr@bullets.co.kr>
2023-11-16 14:25:42 +0900

    섹션 1. 기본 UI 작업하기

---
+++ lib/common/component/custom_text_form_field.dart
@@ -0,0 +1,65 @@
+import 'package:actual_final/common/const/colors.dart';
+import 'package:flutter/material.dart';
+
+class CustomTextFormField extends StatelessWidget {
+  final String? hintText;
+  final String? errorText;
+  final bool obscureText;
+  final bool autofocus;
+  final ValueChanged<String>? onChanged;
+
+  const CustomTextFormField({
+    required this.onChanged,
+    this.autofocus = false,
+    this.obscureText = false,
+    this.hintText,
+    this.errorText,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // default - UnderlineInputBorder
+    final baseBorder = OutlineInputBorder(
+      borderSide: BorderSide(
+        color: INPUT_BORDER_COLOR,
+        width: 1.0,
+      ),
+    );
+
+    return TextFormField(
+      cursorColor: PRIMARY_COLOR,
+      // 비밀번호 입력할때
+      obscureText: obscureText,
+      // 화면에 TextFormField가 들어왔을 때, autofocus
+      autofocus: autofocus,
+      // 값이 바뀔 때 마다 실행되는 콜백
+      onChanged: onChanged,
+      decoration: InputDecoration(
+        // TextFormField 내부 Padding
+        contentPadding: const EdgeInsets.all(20),
+        hintText: hintText,
+        errorText: errorText,
+        hintStyle: const TextStyle(
+          color: BODY_TEXT_COLOR,
+          fontSize: 14.0,
+        ),
+        // 배경색
+        fillColor: INPUT_BG_COLOR,
+        // false - 배경색 없음
+        // true - 배경색 있음
+        filled: true,
+        // 모든 Input 상태의 기본 스타일 세팅
+        border: baseBorder,
+        // 선택되지 않은 상태에서 활성화되어있는 즉, 글자를 쓸 수 있는 상태
+        enabledBorder: baseBorder,
+        // 선택
+        focusedBorder: baseBorder.copyWith(
+          borderSide: baseBorder.borderSide.copyWith(
+            color: PRIMARY_COLOR,
+          ),
+        ),
+      ),
+    );
+  }
+}
